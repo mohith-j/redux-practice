ecf827804791de0df75e7f888eaa5ac465032ac4
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-05-22 02:19:39 -0700

    may 22

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MoveDetails from "./components/MovieDetails/MovieDetails";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">App</div>
+    <div className="App">
+      <Router>
+        <Header></Header>
+        <div className="container">
+          <Routes>
+            <Route path="/" exact Component={Home} />
+            <Route path="/movie/:imdbID" Component={MoveDetails} />
+            <Route path="*" Component={PageNotFound} />
+          </Routes>
+        </div>
+        <Footer></Footer>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/common/Apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+ export const APIKey="3e01213f";
