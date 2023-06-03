d3f28c91e52289afbf180fb5207b82135de18b4f
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-06-03 17:51:46 -0400

    understanding slice

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,16 +2,21 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import { APIKey } from "../../common/Apis/MovieApiKey";
 import MovieApi from "../../common/Apis/MovieApi";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
+
 const Home = () => {
+  const movieText = "Harry";
+  const dispatch=useDispatch();
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async () => {
       const response = await MovieApi.get(
         `?apiKey=${APIKey}&s=${movieText}&type=movie`
       ).catch((err) => {
         console.log("Err:", err);
       });
-      console.log("The response from API", response);
+      // console.log("The response from API", response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
   }, []);
