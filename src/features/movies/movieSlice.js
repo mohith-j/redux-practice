a4671b4da902c935c020d1422352442bb80d3697
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-06-05 15:56:00 -0400

    added thunk middleware

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,19 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { APIKey } from "../../common/Apis/MovieApiKey";
+import MovieApi from "../../common/Apis/MovieApi";
 
-const initialState={
-    movies:{},
-}
+export const fetchAsyncMovies = createAsyncThunk(
+  "movie/fetchAsyncMovies",
+  async () => {
+    const movieText = "Harry";
+    const response = await MovieApi.get(
+      `?apiKey=${APIKey}&s=${movieText}&type=movie`
+    );
+    //    .catch((err) => {
+    //     console.log("Err:", err);
+    //   });
+    // console.log("The response from API", response);
+    // dispatch(addMovies(response.data));
+    return response.data;
+  }
+);
 
-const movieSlice=createSlice({
-    name: "movies",
-    initialState,
-    reducers:{
-        addMovies:(state, {payload})=>{
-            state.movies=payload;
-        },
+const initialState = {
+  movies: {},
+};
+
+const movieSlice = createSlice({
+  name: "movies",
+  initialState,
+  reducers: {
+    addMovies: (state, { payload }) => {
+      state.movies = payload;
     },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fulfilled");
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Rejected");
+    },
+  },
 });
 
-export const { addMovies }=movieSlice.actions;
-export const getAllMovies= (state)=> state.movies.movies
+export const { addMovies } = movieSlice.actions;
+export const getAllMovies = (state) => state.movies.movies;
 export default movieSlice.reducer;
