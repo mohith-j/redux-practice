2eaa07d04ebbecc94ef1531c0a6602b79635243a
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-06-05 23:52:29 -0400

    added carousel

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Slider from "react-slick";
 import { useSelector } from "react-redux";
 import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../../components/MovieCard/MovieCard";
 import "./MovieListing.scss";
+import { Settings } from "../../common/settings";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
@@ -21,9 +23,7 @@ const MovieListing = () => {
     );
   renderShows =
     shows.Response === "True" ? (
-      shows.Search.map((show, index) => (
-        <MovieCard key={index} data={show} />
-      ))
+      shows.Search.map((show, index) => <MovieCard key={index} data={show} />)
     ) : (
       <div className="movies-error">
         <h3>{movies.Error}</h3>
@@ -33,11 +33,15 @@ const MovieListing = () => {
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">{renderMovies}</div>
+        <div className="movie-container">
+          <Slider {...Settings}>{renderMovies}</Slider>
+        </div>
       </div>
       <div className="show-list">
         <h2>Shows</h2>
-        <div className="movie-container">{renderShows}</div>
+        <div className="movie-container">
+          <Slider {...Settings}>{renderShows}</Slider>
+        </div>
       </div>
     </div>
   );
